109d06afb48c1e4c52ae6b6c36aaa37e696f090b
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-01-18 19:29:34 -0500

    user-service microservice added

--- user-service/src/main/java/com/jorge/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/jorge/userservice/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.jorge.discovery;
+package com.jorge.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoveryApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
