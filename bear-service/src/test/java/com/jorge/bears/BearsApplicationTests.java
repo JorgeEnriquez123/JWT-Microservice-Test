c13620b048566b6d2f0bf086f7a275abdb340cf8
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-01-25 14:00:03 -0500

    bear-service base microservice added

---
+++ bear-service/src/test/java/com/jorge/bears/BearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.bears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
