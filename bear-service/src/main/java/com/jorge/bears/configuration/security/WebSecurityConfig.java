17bb2a8c78378e0e6f422fa7bafdb6b6b1afb720
JorgeEnriquez123 <joseph_adagio_7700@hotmail.com>
2024-01-25 21:05:33 -0500

    added Admin-only endpoint in bear service

--- bear-service/src/main/java/com/jorge/bears/configuration/security/WebSecurityConfig.java
+++ bear-service/src/main/java/com/jorge/bears/configuration/security/WebSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
-public class WebConfig{
+public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
--- bear-service/src/main/java/com/jorge/bears/controller/BearController.java
+++ bear-service/src/main/java/com/jorge/bears/controller/BearController.java
@@ -4,7 +4,9 @@ import com.jorge.bears.model.Bear;
 import com.jorge.bears.repository.BearRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,11 @@ public class BearController {
     @GetMapping
     public List<Bear> findAll(){
         return bearRepository.findAll();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Bear saveBear(Bear bear){
+        return bearRepository.save(bear);
     }
 }
